f8992ecf0b17be7b709c6004ec4efff1616f84a6
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2022-11-20 16:21:31 -0400

    firebase config and some fixes to the contacts.

--- src/components/MyContacts.vue
+++ src/components/MyContacts.vue
@@ -2,32 +2,32 @@
   <hr />
   <div class="container">
     <div>
-      <el-link type="primary" href="https://ufred.ca/" target="_blank">
+      <el-link
+        type="primary"
+        href="https://www.linkedin.com/in/lornecyr/"
+        target="_blank"
+      >
         <font-awesome-icon
           icon="fa-brands fa-linkedin"
         />&nbsp;LinkedIn</el-link
       >
     </div>
     <div>
-      <el-link type="primary" href="https://ufred.ca/" target="_blank">
+      <el-link type="primary" href="https://twitter.com/Llourn" target="_blank">
         <font-awesome-icon
           icon="fa-brands fa-twitter-square"
         />&nbsp;Twitter</el-link
       >
     </div>
     <div>
-      <el-link type="primary" href="https://ufred.ca/">
+      <el-link type="primary" href="https://github.com/Llourn" target="_blank">
         <font-awesome-icon
           icon="fa-brands fa-github-square"
         />&nbsp;Github</el-link
       >
     </div>
     <div>
-      <el-link
-        type="primary"
-        href="mailto:Lorne.S.Cyr@gmail.com"
-        target="_blank"
-      >
+      <el-link type="primary" href="mailto:Lorne.S.Cyr@gmail.com">
         <font-awesome-icon icon="fas fa-envelope-square" />&nbsp;Email</el-link
       >
     </div>
