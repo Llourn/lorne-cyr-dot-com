2270ff2d655f5a20a92f82af5526496250a479e8
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2022-11-20 09:32:29 -0400

    Working on the project cards

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card :body-style="{ padding: '0px' }">
+    <img :src="imageWithPath" class="image" />
+    <div style="padding: 14px">
+      <span>{{ props.project.title }}</span>
+      <div class="bottom">
+        <el-button text class="button">Operating</el-button>
+        <div class="description" v-html="props.project.description"></div>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+  import type { Project } from "@/models/ProjectModel";
+  import { computed } from "vue";
+
+  export interface ProjectCardProps {
+    project: Project;
+  }
+
+  const props = defineProps<ProjectCardProps>();
+
+  const imageWithPath = computed(() => {
+    return `src/assets/${props.project.image}`;
+  });
+</script>
+
+<style lang="scss">
+  .image {
+    width: 100%;
+    display: block;
+  }
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,14 +1,30 @@
 <template>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
-  <h1>Projects</h1>
+  <div class="projects-container">
+    <project-card
+      v-for="(project, index) in projects"
+      :project="project"
+      :key="index"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from "vue";
+  import projectData from "../assets/data/projects.json";
+  import ProjectCard from "@/components/ProjectCard.vue";
 
-<style lang="scss"></style>
+  const projects = ref(projectData.projects);
+</script>
+
+<style lang="scss" scoped>
+  .projects-container {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: space-evenly;
+    > * {
+      max-width: 400px;
+    }
+  }
+</style>
